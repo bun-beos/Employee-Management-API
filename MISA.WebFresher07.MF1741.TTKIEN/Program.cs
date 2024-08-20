11f65b78839d20abbc3309b7b5e74f8d43584fa9
ttkien0311 <trantrungkien532@hmail.com>
2024-08-20 09:44:40 +0700

    add UseCors

--- MISA.WebFresher07.MF1741.TTKIEN/Program.cs
+++ MISA.WebFresher07.MF1741.TTKIEN/Program.cs
@@ -59,11 +59,12 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("corsapp");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseCors("corsapp");
 }
 
 app.UseHttpsRedirection();
