6510673a2bfcf22ae9e3f57032df750d22a64680
ttkien0311 <trantrungkien532@hmail.com>
2024-08-20 09:03:59 +0700

    add server controller

---
+++ MISA.WebFresher07.MF1741.TTKIEN/Controllers/ServersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MISA.WebFresher07.MF1741.TTKIEN.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class ServersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult ServerAsync()
+        {
+            return Ok($"Server is running now {DateTime.Now}");
+        } 
+    }
+}
